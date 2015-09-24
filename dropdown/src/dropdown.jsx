513365109e61bfef82b6a0d0d5ca2462038ba814
Francisco De La Cruz <helloquico@gmail.com>
2015-09-24 18:19:54 -0400

    Exploring State and Events - Select Items

--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -6,19 +6,29 @@ module.exports = React.createClass({
 	handleClick: function () {
 		this.setState({open: !this.state.open});
 	},
+	handleItemClick: function (item) {
+		this.setState({
+			open: false,
+			itemTitle: item
+		});
+	},
 	getInitialState: function () {
 		return { open: false }
 	},
 	render: function () {
 		var list = this.props.items.map(function (item) {
-			return <ListItem item={item} />
-		});
+			return <ListItem 
+								item={item} 
+								whenItemClicked={this.handleItemClick} 
+								className={this.state.itemTitle === item ? "active" : ""}
+							/>
+		}.bind(this));
 
 		return <div className="dropdown">
 			<Button 
 				whenClicked={this.handleClick} 
 				className="btn-default" 
-				title={this.props.title} 
+				title={this.state.itemTitle || this.props.title} 
 				subTitleClassName="caret" 
 				/>
 			<ul className={"dropdown-menu " + (this.state.open ? "show" : "")}>
