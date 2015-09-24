bdfef51ffb5040b579de9d9d1f9799ca01612e34
Francisco De La Cruz <helloquico@gmail.com>
2015-09-24 15:04:02 -0400

    Exploring State and Events - Add Content

--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -1,13 +1,16 @@
 var React = require('react');
 var Button = require('./button');
-// var List = require('./list');
+var ListItem = require('./list-item');
 
 module.exports = React.createClass({
 	handleClick: function () {
 		alert('hello from dropdown');
 	},
 	render: function () {
-		
+		var list = this.props.items.map(function (item) {
+			return <ListItem item={item} />
+		});
+
 		return <div className="dropdown">
 			<Button 
 				whenClicked={this.handleClick} 
@@ -15,6 +18,9 @@ module.exports = React.createClass({
 				title={this.props.title} 
 				subTitleClassName="caret" 
 				/>
+			<ul>
+				{list}
+			</ul>
 		</div>
 	}
 });
