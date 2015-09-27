93bd9587eac4f3e5676e0e5e1a17d5df38835931
Francisco De La Cruz <helloquico@gmail.com>
2015-09-26 21:35:15 -0400

    Firebase - Scaffold The Header

--- todos/src/app.jsx
+++ todos/src/app.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactFire = require('reactfire');
 var FireBase = require('firebase');
+var Header = require('./header');
 var rootUrl = 'https://thesetodos.firebaseio.com/';
 
 var App = React.createClass({
@@ -10,12 +11,15 @@ var App = React.createClass({
 		// this.state.items => {}
 	},
   render: function() {
-		console.log(this.state);
-
-    return <h1>
-      Hello, React!
-    </h1>
-  }
+    return <div className="row panel panel-default">
+			<div className="col-md-8 col-md-offset-2">
+				<h2 className="text-center">
+					To-Do List
+				</h2>
+				<Header />
+			</div>
+		</div>
+	}
 });
 
 var element = React.createElement(App, {});
