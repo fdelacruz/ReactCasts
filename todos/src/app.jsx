0421602b386ae656745b57182826d557dfbb75c8
Francisco De La Cruz <helloquico@gmail.com>
2015-09-27 15:10:57 -0400

    Firebase - Bulk Delete Data

--- todos/src/app.jsx
+++ todos/src/app.jsx
@@ -14,9 +14,9 @@ var App = React.createClass({
 		}
 	},
 	componentWillMount: function () {
-		fb = new FireBase(rootUrl + 'items/');
-		this.bindAsObject(fb, 'items');
-		fb.on('value', this.handleDataLoaded);
+		this.fb = new FireBase(rootUrl + 'items/');
+		this.bindAsObject(this.fb, 'items');
+		this.fb.on('value', this.handleDataLoaded);
 	},
   render: function() {
     return <div className="row panel panel-default">
@@ -28,12 +28,35 @@ var App = React.createClass({
 				<hr />
 				<div className={"content " + (this.state.loaded ? 'loaded' : '')}>
 					<List items={this.state.items} />
+					{this.deleteButton()}
 				</div>
 			</div>
 		</div>
 	},
 	handleDataLoaded: function () {
 		this.setState({loaded: true});
+	},
+	deleteButton: function () {
+		if(!this.state.items) {
+			return
+		} else {
+			return <div className="text-center clear-complete">
+				<hr />
+				<button
+					type="button"
+					onClick={this.onDeleteDoneClick}
+					className="btn btn-default">
+					Clear Complete
+				</button>
+			</div>
+		}
+	},
+	onDeleteDoneClick: function () {
+		for(var key in this.state.items) {
+			if (this.state.items[key].done === true) {
+				this.fb.child(key).remove()
+			}
+		}
 	}
 });
 
