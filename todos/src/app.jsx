103a91e6b4e59b6aa746a8154eb7b84d0cf47c66
Francisco De La Cruz <helloquico@gmail.com>
2015-09-26 21:05:20 -0400

    Firebase - Hook Up Our Remote DataStore

--- todos/src/app.jsx
+++ todos/src/app.jsx
@@ -1,9 +1,17 @@
 var React = require('react');
 var ReactFire = require('reactfire');
 var FireBase = require('firebase');
+var rootUrl = 'https://thesetodos.firebaseio.com/';
 
 var App = React.createClass({
+	mixins: [ReactFire],
+	componentWillMount: function () {
+		this.bindAsObject(new FireBase(rootUrl + 'items/'), 'items');
+		// this.state.items => {}
+	},
   render: function() {
+		console.log(this.state);
+
     return <h1>
       Hello, React!
     </h1>
