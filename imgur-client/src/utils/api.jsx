a9ce211e30e77ccfd6d900f92995e45d1bb6cacd
Francisco De La Cruz <helloquico@gmail.com>
2015-10-01 19:36:00 -0400

    Work with Fetch's Promises

--- imgur-client/src/utils/api.jsx
+++ imgur-client/src/utils/api.jsx
@@ -2,12 +2,15 @@ var Fetch = require('whatwg-fetch');
 var rootUrl = 'https://api.imgur.com/3/';
 var apiKey = '2fcb3582fded9dc';
 
-module.exports = {
+module.exports = window.api = {
 	get: function (url) {
 		return fetch(rootUrl + url, {
 			headers: {
 				'Authorization': 'Client-ID ' + apiKey
 			}
+		})
+		.then(function (response) {
+			return response.json();
 		});
 	}
 };
