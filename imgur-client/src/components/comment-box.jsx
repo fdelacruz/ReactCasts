851ba4723ced5a1209ccfc615ee5529bdb031fa4
Francisco De La Cruz <helloquico@gmail.com>
2015-10-08 17:22:49 -0400

    CSS Animations

--- imgur-client/src/components/comment-box.jsx
+++ imgur-client/src/components/comment-box.jsx
@@ -2,9 +2,17 @@ var React = require('react');
 
 module.exports = React.createClass({
 	render: function () {
-		return <div>
-			I am a comment box.
-			{this.props.comments}
+		return <div className="list-group">
+			{this.renderComments()}
 		</div>
+	},
+	renderComments: function () {
+		return this.props.comments.slice(0, 20).map(function (comment) {
+			return <li className="list-group-item" key={comment.id}>
+				<span className="badge">{comment.ups}</span>
+				<h5>{comment.author}</h5>
+				{comment.comment}
+			</li>
+		})
 	}
 });
